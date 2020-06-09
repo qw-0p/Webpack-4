15015afca3bbb407a631b69a2e8d56b649215a22
Aleksandr Sarkisov <quopiks@gmail.com>
2020-06-09 19:59:40 +0300

    added plugin jquery, mini-css-extract etc

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -12,12 +14,35 @@ module.exports = {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
     },
+    resolve: {
+        extensions: ['.js'], //Default extensions
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+    devServer: {
+        port: 8800,
+    },
     plugins: [
         new HTMLWebpackPlugin({
             title: '',
             template: './index.html',
         }),
         new CleanWebpackPlugin(),
+        // new CopyWebpackPlugin([
+        //     {
+        //         from: path.resolve(__dirname, ''),
+        //         to: path.resolve(__dirname, ''),
+        //     },
+        // ]),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
     ],
     module: {
         rules: [
