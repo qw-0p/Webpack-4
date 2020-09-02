1a5891c5fac3a6932589b562b3b64febe3bd0e67
Aleksandr Sarkisov <quopiks@gmail.com>
2020-09-02 15:19:06 +0300

    configurated babel, added rules

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,17 @@ module.exports = {
         path: path.resolve(__dirname, './dist'),
         publicPath: '/dist'
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            }
+        ]
+    },
     devServer: {
-        overlay: true
+        overlay: true,
+        port: 4400
     }
 };
