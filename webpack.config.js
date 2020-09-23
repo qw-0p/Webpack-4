5fc06a906e1b315e096979d16af2c2a540648857
Aleksandr Sarkisov <quopiks@gmail.com>
2020-09-23 09:59:09 +0300

    added some rules for eslint, prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,133 +1,139 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserWebpackPlugin = require('terser-webpack-plugin');
-const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
 
-const isDev = process.env.NODE_ENV === 'development';
-const isProd = !isDev;
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
 
 const optimization = () => {
     const config = {
         splitChunks: {
-            chunks: 'all'
-        }
-    };
-
-    if (isProd) {
-        config.minimizer = [new OptimizeCssAssetWebpackPlugin(), new TerserWebpackPlugin()];
+            chunks: 'all',
+        },
     }
 
-    return config;
-};
+    if (isProd) {
+        config.minimizer = [
+            new OptimizeCssAssetWebpackPlugin(),
+            new TerserWebpackPlugin(),
+        ]
+    }
 
-const filename = (ext) => (isDev ? `[name].${ext}` : `[name].[hash].${ext}`);
+    return config
+}
 
-const cssLoaders = (extra) => {
+const filename = ext => (isDev ? `[name].${ext}` : `[name].[hash].${ext}`)
+
+const cssLoaders = extra => {
     const loaders = [
         {
             loader: MiniCssExtractPlugin.loader,
             options: {
                 hmr: isDev,
-                reloadAll: true
-            }
+                reloadAll: true,
+            },
         },
-        'css-loader',
-        'postcss-loader'
-    ];
+        {
+            loader: 'css-loader',
+            options: {modules: true},
+        },
+        'postcss-loader',
+    ]
 
     if (extra) {
-        loaders.push(extra);
+        loaders.push(extra)
     }
 
-    return loaders;
-};
+    return loaders
+}
 
-const babelOptions = (preset) => {
+const babelOptions = preset => {
     const opts = {
         presets: ['@babel/preset-env'],
-        plugins: ['@babel/plugin-proposal-class-properties']
-    };
-
-    if (preset) {
-        opts.presets.push(preset);
+        plugins: ['@babel/plugin-proposal-class-properties'],
     }
 
-    return opts;
-};
+    if (preset) {
+        opts.presets.push(preset)
+    }
+
+    return opts
+}
 
 const jsLoaders = () => {
     const loaders = [
         {
             loader: 'babel-loader',
-            options: babelOptions()
-        }
-    ];
+            options: babelOptions(),
+        },
+    ]
 
     if (isDev) {
-        loaders.push('eslint-loader');
+        loaders.push('eslint-loader')
     }
 
-    return loaders;
-};
+    return loaders
+}
 
 const plugins = () => {
     const base = [
         new HTMLWebpackPlugin({
             template: './index.html',
             minify: {
-                collapseWhitespace: isProd
-            }
+                collapseWhitespace: isProd,
+            },
         }),
         new CleanWebpackPlugin(),
         new CopyWebpackPlugin({
             patterns: [
                 {
                     from: path.resolve(__dirname, 'src/static'),
-                    to: path.resolve(__dirname, 'dist/static')
+                    to: path.resolve(__dirname, 'dist/static'),
                 },
                 {
                     from: path.resolve(__dirname, 'src/assets/img'),
-                    to: path.resolve(__dirname, 'dist/assets/img')
-                }
-            ]
+                    to: path.resolve(__dirname, 'dist/assets/img'),
+                },
+            ],
         }),
         new MiniCssExtractPlugin({
-            filename: filename('css')
-        })
-    ];
+            filename: filename('css'),
+        }),
+    ]
 
     if (isProd) {
-        base.push(new BundleAnalyzerPlugin());
+        base.push(new BundleAnalyzerPlugin())
     }
 
-    return base;
-};
+    return base
+}
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
     entry: {
-        main: ['@babel/polyfill', './index.js']
+        main: ['@babel/polyfill', './index.js'],
     },
     output: {
         filename: filename('js'),
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
     },
     resolve: {
         extensions: ['.js', '.json', '.png'],
         alias: {
             '@models': path.resolve(__dirname, 'src/models'),
-            '@': path.resolve(__dirname, 'src')
-        }
+            '@': path.resolve(__dirname, 'src'),
+        },
     },
     optimization: optimization(),
     devServer: {
         port: 4200,
-        hot: isDev
+        hot: isDev,
     },
     devtool: isDev ? 'cheap-eval-source-map' : '',
     plugins: plugins(),
@@ -135,53 +141,53 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/,
-                use: cssLoaders()
+                use: cssLoaders(),
             },
             {
                 test: /\.less$/,
-                use: cssLoaders('less-loader')
+                use: cssLoaders('less-loader'),
             },
             {
                 test: /\.s[ac]ss$/,
-                use: cssLoaders('sass-loader')
+                use: cssLoaders('sass-loader'),
             },
             {
                 test: /\.(png|jpg|svg|gif)$/,
-                use: ['file-loader']
+                use: ['file-loader'],
             },
             {
                 test: /\.(ttf|woff|woff2|eot)$/,
-                use: ['file-loader']
+                use: ['file-loader'],
             },
             {
                 test: /\.xml$/,
-                use: ['xml-loader']
+                use: ['xml-loader'],
             },
             {
                 test: /\.csv$/,
-                use: ['csv-loader']
+                use: ['csv-loader'],
             },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: jsLoaders()
+                use: jsLoaders(),
             },
             {
                 test: /\.ts$/,
                 exclude: /node_modules/,
                 loader: {
                     loader: 'babel-loader',
-                    options: babelOptions('@babel/preset-typescript')
-                }
+                    options: babelOptions('@babel/preset-typescript'),
+                },
             },
             {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
                 loader: {
                     loader: 'babel-loader',
-                    options: babelOptions('@babel/preset-react')
-                }
-            }
-        ]
-    }
-};
+                    options: babelOptions('@babel/preset-react'),
+                },
+            },
+        ],
+    },
+}
